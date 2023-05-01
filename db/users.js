3f7d69b0ef959f1943073444d5c68790ca822fa4
Anna Gibes <gibesav@gmail.com>
2023-05-01 13:59:54 -0500

    getUser complete

--- db/users.js
+++ db/users.js
@@ -27,15 +27,58 @@ async function createUser({
 }
 
 async function getUser({ username, password }) {
-
+  try {
+    const user = await getUserByUsername(username);
+    const hashedPassword = user.password;
+    let passwordsMatch = await bcrypt.compare(password, hashedPassword);
+    if (!passwordsMatch) {
+      console.log('passwords do not match');
+      return;
+    } else {
+      const {rows: [user] } = await client.query(`
+        SELECT id, username FROM users;
+      `);
+      return user;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getUserById(userId) {
+  try {
+    const {rows: [user]} = await client.query(`
+      SELECT id, username FROM users
+      WHERE id=$1
+    `, [userId]);
 
+    if (user.length === 0) {
+      console.log('could not find user');
+      return;
+    }
+
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getUserByUsername(userName) {
-
+  try {
+    const {rows: [user]} = await client.query(`
+      SELECT id, username, password FROM users
+      WHERE username=$1
+    `, [userName]);
+  
+    if (user.length === 0) {
+      console.log('could not find user');
+      return;
+    }
+    
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
