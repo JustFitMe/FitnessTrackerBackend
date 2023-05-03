54b9196ce18c16b673c25505b9f18500c0b26561
Aubrey042 <127706254+Aubrey042@users.noreply.github.com>
2023-05-03 00:20:22 -0400

    routine_activities.js Setup

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -1,13 +1,35 @@
-const client = require("./client");
+const client = require('./client');
 
 async function addActivityToRoutine({
   routineId,
   activityId,
   count,
   duration,
-}) {}
+}) {
+  try {
+    const {rows: [activity]} = await client.query(`
+      INSERT INTO routine_activities("routineId", "activityId", count, duration)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `, [routineId, activityId, count, duration]);
+    return activity;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-async function getRoutineActivityById(id) {}
+async function getRoutineActivityById(id) {
+  try {
+    const { rows: [routineActivity] } = await client.query(`
+      SELECT * FROM routine_activities
+      WHERE id = $1;
+    `, [id]);
+
+    return routineActivity;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function getRoutineActivitiesByRoutine({ id }) {}
 
