2d85182ae21d408d5148c1d918e5a6cd87926fd4
Anna Gibes <gibesav@gmail.com>
2023-05-08 10:53:32 -0500

    attach single activity to routine

--- api/routines.js
+++ api/routines.js
@@ -34,8 +34,12 @@ router.patch('/:routineId', requireUser, async (req, res, next) => {
             const updatedRoutine = await updateRoutine({id: routineId, creatorId, isPublic, name, goal})
             res.send(updatedRoutine);
         } else {
-            // res.status = 403;
-            // res.send(res.status);
+            res.status(403);
+            next({
+                error: 'error',
+                message: 'User ' + req.user.username + ' is not allowed to update Every day',
+                name: 'UpdateError'
+            }); 
         }
     } catch (error) {
         next(error);
@@ -47,11 +51,15 @@ router.delete('/:routineId', requireUser, async (req, res, next) => {
     const routine = await getRoutineById(routineId);
     try {
         if (routine && routine.creatorId == req.user.id) {
-            //await destroyRoutine(routineId);
-            // res.send(await destroyRoutine(routineId));
+            await destroyRoutine(routineId);
+            res.send(routine);
         } else {
-            // res.status = 403;
-            // res.send(res.status);
+            res.status(403);
+            next({
+                error: 'error',
+                message: 'User ' + req.user.username + ' is not allowed to delete On even days',
+                name: 'UpdateError'
+            }); 
         }
     } catch (error) {
         next(error);
@@ -62,19 +70,21 @@ router.post('/:routineId/activities', requireUser, async (req, res, next) => {
     try {
 
         const {routineId} = req.params;
-        console.log('r',{id: routineId})
+        // console.log('r',{id: routineId})
         
         const {activityId, count, duration} = req.body;
-        console.log('activityID',activityId)
-        const activityToAdd = await getActivityById(activityId);
-        console.log('activity',activityToAdd)
+        // console.log('activityID', activityId)
+        // const activityToAdd = await getActivityById(activityId);
+        // console.log('activity', activityToAdd)
         
-        console.log('routineId',routineId)
-        const _routine = await getRoutineById(routineId);
-        console.log('routine',_routine)
+        // console.log('routineId',routineId)
+        // const _routine = await getRoutineById(routineId);
+        // console.log('routine',_routine)
 
-       
-        // if (duplication on (routineId, activityId) pair) {
+        const foundRoutineActivities = await getRoutineActivitiesByRoutine({id: routineId});
+        const existingRoutineActivities = foundRoutineActivities && foundRoutineActivities.filter(routineActivity => routineActivity.activityId === activityId);
+
+        if (existingRoutineActivities && existingRoutineActivities.length) {
         
             next({
                 error: 'error',
@@ -83,14 +93,22 @@ router.post('/:routineId/activities', requireUser, async (req, res, next) => {
             }); 
             
        
-        // } else {
+        } else {
 
             const routine = await addActivityToRoutine({routineId, activityId, count, duration});
-            console.log(routine);
-            res.send(routine);
+            if (routine) {
 
-        // }
-        
+                console.log(routine);
+                res.send(routine);
+            } else {
+                next({
+                    error: 'error',
+                    message: 'Activity ID ' + activityId + ' already exists in Routine ID ' + routineId,
+                    name: 'ActivityExistsError'
+                }); 
+            }
+
+        }
 
     } catch  (error) {
         next(error);
