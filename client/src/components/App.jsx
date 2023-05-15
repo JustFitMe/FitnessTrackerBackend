11b0f4ca48c3a4cd8207ce50cb4d31d875481d52
Aubrey042 <127706254+Aubrey042@users.noreply.github.com>
2023-05-14 20:18:09 -0400

    User Token Reset, Index.js Updated

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,56 +1,169 @@
-import React, { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import {Header, Routines, Home, Register, Login, Profile, MyRoutines, SingleRoutine} from './index';
-import { getPublicRoutines, getUserRoutines } from '../api/api';
-import Activities from './Activities';
-
-
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import {
+  Header,
+  Routines,
+  Home,
+  Register,
+  Login,
+  Profile,
+  MyRoutines,
+  SingleRoutine,
+} from "./index";
+import { getPublicRoutines, getUserRoutines } from "../api/api";
+import Activities from "./Activities.jsx";
 
 const App = () => {
-    // const [routines, setRoutines] = useState([]);
-    const [publicRoutines, setPublicRoutines] = useState([])
-    const [user, setUser] = useState([]);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [token, setToken] = useState('');
-    const [singleRoutine, setSingleRoutine] = useState([]);
-    const [name, setName] = useState('');
-    const [goal, setGoal] = useState('');    
-    const [isPublic, setIsPublic] = useState(false);  
-    // const [userRoutines, setUserRoutines] = useState([]);
-    useEffect(() => {
+  // const [routines, setRoutines] = useState([]);
+  const [publicRoutines, setPublicRoutines] = useState([]);
+  const [user, setUser] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
+  const [singleRoutine, setSingleRoutine] = useState([]);
+  const [name, setName] = useState("");
+  const [goal, setGoal] = useState("");
+  const [isPublic, setIsPublic] = useState(false);
+  // const [userRoutines, setUserRoutines] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const fetchedRoutines = await getPublicRoutines();
+      // const userRoutines = await getUserRoutines(token);
+      // console.log('userRoutines--->', userRoutines)
+      // setPublicRoutines(fetchedRoutines.filter(routine => routine.isPublic))
 
-        const getData = async () => {
-            const fetchedRoutines = await getPublicRoutines();
-            // const userRoutines = await getUserRoutines(token);
-            // console.log('userRoutines--->', userRoutines)
-            // setPublicRoutines(fetchedRoutines.filter(routine => routine.isPublic))
+      setPublicRoutines(fetchedRoutines);
+      // setUserRoutines(userRoutines);
+    };
+    getData();
+  }, []);
+  //    console.log(publicRoutines);
 
-            setPublicRoutines(fetchedRoutines);
-            // setUserRoutines(userRoutines);
-        }
-        getData();
-}, [])
-    //    console.log(publicRoutines);
-       
-return (
-
+  return (
     <div>
-        <Header token={token} setToken={setToken} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
-        <Routes>
-            <Route path='/' element={<Home user={user} isLoggedIn={isLoggedIn} setToken={setToken} setUser={setUser} setIsLoggedIn={setIsLoggedIn} username={username} setUsername={setUsername} password={password} setPassword={setPassword} />}></Route>
-            <Route path='/register' element={<Register user={user} isLoggedIn={isLoggedIn} setToken={setToken} setUser={setUser} setIsLoggedIn={setIsLoggedIn} username={username} setUsername={setUsername} password={password} setPassword={setPassword} />}></Route>
-            <Route path='/login' element={<Login token={token} user={user} isLoggedIn={isLoggedIn} setToken={setToken} setUser={setUser} setIsLoggedIn={setIsLoggedIn} username={username} setUsername={setUsername} password={password} setPassword={setPassword} />}></Route>
-            <Route path='/me' element={<Profile user={user} isLoggedIn={isLoggedIn} setToken={setToken} setUser={setUser} setIsLoggedIn={setIsLoggedIn} username={username} setUsername={setUsername} password={password} setPassword={setPassword} />}></Route>
-            <Route path='/routines' element={<Routines name={name} goal={goal} setIsPublic={setIsPublic} isPublic={isPublic} setGoal={setGoal} setName={setName} token={token} publicRoutines={publicRoutines} setPublicRoutines={setPublicRoutines} user={user} isLoggedIn={isLoggedIn}/>}></Route>
-            <Route path='/:username/routines' element={<MyRoutines setSingleRoutine={setSingleRoutine} isLoggedIn={isLoggedIn} user={user} token={token}/>}></Route>
-            <Route path='/activities' element={<Activities />}></Route>
-            <Route path='/:routineId' element={<SingleRoutine user={user} token={token} isPublic={isPublic} setIsPublic={setIsPublic} name={name} goal={goal} isLoggedIn={isLoggedIn} singleRoutine={singleRoutine} setGoal={setGoal} setName={setName}/>}></Route>
-        </Routes>
+      <Header
+        token={token}
+        setToken={setToken}
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+      />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              user={user}
+              isLoggedIn={isLoggedIn}
+              setToken={setToken}
+              setUser={setUser}
+              setIsLoggedIn={setIsLoggedIn}
+              username={username}
+              setUsername={setUsername}
+              password={password}
+              setPassword={setPassword}
+            />
+          }
+        ></Route>
+        <Route
+          path="/register"
+          element={
+            <Register
+              user={user}
+              isLoggedIn={isLoggedIn}
+              setToken={setToken}
+              setUser={setUser}
+              setIsLoggedIn={setIsLoggedIn}
+              username={username}
+              setUsername={setUsername}
+              password={password}
+              setPassword={setPassword}
+            />
+          }
+        ></Route>
+        <Route
+          path="/login"
+          element={
+            <Login
+              token={token}
+              user={user}
+              isLoggedIn={isLoggedIn}
+              setToken={setToken}
+              setUser={setUser}
+              setIsLoggedIn={setIsLoggedIn}
+              username={username}
+              setUsername={setUsername}
+              password={password}
+              setPassword={setPassword}
+            />
+          }
+        ></Route>
+        <Route
+          path="/me"
+          element={
+            <Profile
+              user={user}
+              isLoggedIn={isLoggedIn}
+              setToken={setToken}
+              setUser={setUser}
+              setIsLoggedIn={setIsLoggedIn}
+              username={username}
+              setUsername={setUsername}
+              password={password}
+              setPassword={setPassword}
+            />
+          }
+        ></Route>
+        <Route
+          path="/routines"
+          element={
+            <Routines
+              name={name ?? ""}
+              goal={goal ?? ""}
+              setIsPublic={setIsPublic}
+              isPublic={isPublic ?? ""}
+              setGoal={setGoal}
+              setName={setName}
+              token={token ?? ""}
+              publicRoutines={publicRoutines ?? ""}
+              setPublicRoutines={setPublicRoutines}
+              user={user ?? ""}
+              isLoggedIn={isLoggedIn ?? ""}
+            />
+          }
+        ></Route>
+        <Route
+          path="/:username/routines"
+          element={
+            <MyRoutines
+              setSingleRoutine={setSingleRoutine}
+              isLoggedIn={isLoggedIn}
+              user={user}
+              token={token}
+            />
+          }
+        ></Route>
+        <Route path="/activities" element={<Activities />}></Route>
+        <Route
+          path="/:routineId"
+          element={
+            <SingleRoutine
+              user={user}
+              token={token}
+              isPublic={isPublic}
+              setIsPublic={setIsPublic}
+              name={name}
+              goal={goal}
+              isLoggedIn={isLoggedIn}
+              singleRoutine={singleRoutine}
+              setGoal={setGoal}
+              setName={setName}
+            />
+          }
+        ></Route>
+      </Routes>
     </div>
-
-)
-}
+  );
+};
 
 export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,67 +1,82 @@
-import React from 'react';
-import {NavLink, useNavigate} from 'react-router-dom';
-import './Header.css';
+import React, { useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "./Header.css";
 
-const Header = ({isLoggedIn, setToken, setIsLoggedIn, token}) => {
-    const navigate = useNavigate();
+const Header = ({ isLoggedIn, setToken, setIsLoggedIn, token }) => {
+  const [isUser, setuser] = useState(false);
+  const navigate = useNavigate();
 
-    const handleLogin = async (event) => {
-        event.preventDefault();
-        navigate('/login');
-        // const userToAuth={user:{username,password}};
-        // const data = await loginUser(userToAuth);
-        // if (data.token) {
-        //     setToken(data.token);
-        //     setUser(data);
-        //     setIsLoggedIn(true);
-        // }
-    }
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    navigate("/login");
+    // const userToAuth={user:{username,password}};
+    // const data = await loginUser(userToAuth);
+    // if (data.token) {
+    //     setToken(data.token);
+    //     setUser(data);
+    //     setIsLoggedIn(true);
+    // }
+  };
 
-    const handleRegister = async(event) => {
-        event.preventDefault();
-        navigate('/register');
-        // const userToAuth={user:{username,password}};
-        // const data = createUser(userToAuth);
-        // if (data.token) {
-        //     setToken(data.token);
-        //     setUser(data);
-        //     setIsLoggedIn(true);
-        }
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    navigate("/register");
+    // const userToAuth={user:{username,password}};
+    // const data = createUser(userToAuth);
+    // if (data.token) {
+    //     setToken(data.token);
+    //     setUser(data);
+    //     setIsLoggedIn(true);
+  };
 
-    const handleLogout = async(event) => {
-        event.preventDefault();
-        localStorage.removeItem('token');
-        setToken('');
-        setIsLoggedIn(false);
-        console.log(token);
-    }
+  const handleLogout = async (event) => {
+    event.preventDefault();
+    localStorage.removeItem("token");
+    setToken("");
+    setIsLoggedIn(false);
+    console.log(token);
+  };
 
-    return (
-        <div id='header'>
-            <h1>Fitness Tracker</h1>
-            <nav>
-                
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/routines">Routines</NavLink>
-                <NavLink to="/activities">Activities</NavLink>
-                {/* <NavLink to="/register">Sign Up</NavLink>
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) setuser(true);
+    else setuser(false);
+  }, []);
+
+  useEffect(() => {
+    if (isUser) setIsLoggedIn(true);
+  }, [isUser]);
+
+  return (
+    <div id="header">
+      <h1>Fitness Tracker</h1>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/routines">Routines</NavLink>
+        <NavLink to="/activities">Activities</NavLink>
+        {/* <NavLink to="/register">Sign Up</NavLink>
                 <NavLink to="/login">Sign In</NavLink> */}
-                <>
-            {isLoggedIn ? 
+        <>
+          {isLoggedIn ? (
             <>
-            <button type="submit" onClick={handleLogout}>Logout</button>
+              <button type="submit" onClick={handleLogout}>
+                Logout
+              </button>
             </>
-            :
+          ) : (
             <>
-            <button type="submit" onClick={handleLogin}>Login</button>
-            <button type="submit" onClick={handleRegister}>Register</button>
+              <button type="submit" onClick={handleLogin}>
+                Login
+              </button>
+              <button type="submit" onClick={handleRegister}>
+                Register
+              </button>
             </>
-        }
+          )}
         </>
-
-            </nav>
-        </div>
-    )
-}
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
